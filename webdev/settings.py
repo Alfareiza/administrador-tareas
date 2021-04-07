55b7b859aa4e5abadde5f37a9f9d2d5979770d2d
alfonso <@lfareiza1989>
2021-04-07 14:38:32 -0300

    Corregido bug de ALLOWED_HOSTS

--- webdev/settings.py
+++ webdev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)e6@+ih!=hgvwi^zxh*!ht&6o3e7b71%exy-&(-*sl#y%kh0dd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
