110b4d5376802e9f07c97d4a3b24873193917404
alfonso <@lfareiza1989>
2021-04-22 10:57:43 -0300

    Creada función que trata GET o POST del formulario de registro de tareas.

---
+++ webdev/tareas/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+# Create your views here.
+from webdev.tareas.forms import TareaNuevaForm
+
+
+def home(request):
+    if request.method == 'POST':
+        form = TareaNuevaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return render(request, 'tareas/home.html', {'form': form}, status=400)
+
+    return render(request, 'tareas/home.html')
