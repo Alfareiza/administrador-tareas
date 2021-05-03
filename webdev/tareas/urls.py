f2e4b6b5b488596c74d9ec56a23d0161b6eaef33
Alfonso <alfareiza@gmail.com>
2021-05-03 19:26:19 -0300

    Agregada url para detalle de tarea. tareas:detalle

--- webdev/tareas/urls.py
+++ webdev/tareas/urls.py
@@ -5,5 +5,6 @@ from webdev.tareas import views
 app_name = 'tareas'
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('<int:tarea_id>', views.detalle, name='detalle')
 ]
