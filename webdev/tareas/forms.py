da682639aeeded8b751b12b44b2d482d6cc02ae3
Alfonso <alfareiza@gmail.com>
2021-05-03 19:27:15 -0300

    Agregado formulario para tratar tareas existentes.

--- webdev/tareas/forms.py
+++ webdev/tareas/forms.py
@@ -4,6 +4,20 @@ from webdev.tareas.models import Tarea
 
 
 class TareaNuevaForm(ModelForm):
+    """
+    Este formulario va a tratar la entrada de tareas, solamente capturando su nombre.
+    """
+
     class Meta:
         model = Tarea
         fields = ['nombre']
+
+
+class TareaForm(ModelForm):
+    """
+    Este formulario va a tratar todos los campos de la tarea existente.
+    """
+
+    class Meta:
+        model = Tarea
+        fields = ['nombre', 'realizada']
