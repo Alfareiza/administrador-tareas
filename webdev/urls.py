35bf6b08664e2532d2ca0c7402b2719bc6ab0072
Alfonso <alfareiza@gmail.com>
2021-05-08 14:59:06 -0300

    configurado django_debug_toolbar

--- webdev/urls.py
+++ webdev/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 from webdev import settings
-from webdev.home_view import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
